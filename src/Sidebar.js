adc0957701bc37125ee88140ef45222144b80ed5
Gracious Nguwo <graciousnguwo2020@gmail.com>
2022-06-06 23:28:49 +0200

    sidebar modified

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,6 +3,14 @@ import React from "react";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+
+  const recentItem = (topic) => {
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar__top">
@@ -19,13 +27,19 @@ const Sidebar = () => {
         </div>
 
         <div className="sidebar__stat">
-          <p>Who viewed you</p>
-          <p className="siderbar__statNumber">2,789</p>
+          <p>Who viewed post</p>
+          {recentItem("programming")}
+          <p className="sidebar__statNumber">2,789</p>
         </div>
       </div>
 
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem("softwareengineering")}
+        {recentItem("softwaredeveloper")}
+        {recentItem("softwaredesign")}
+        {recentItem("webdevelopment")}
       </div>
     </div>
   );
