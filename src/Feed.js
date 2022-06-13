c7c40e852045f99fd543b45d0a5627f03dfa56d5
Gracious Nguwo <graciousnguwo2020@gmail.com>
2022-06-13 14:25:24 +0200

    post section added

--- src/Feed.js
+++ src/Feed.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import CreateIcon from "@mui/icons-material/Create";
 import ImageIcon from "@mui/icons-material/Image";
 import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
@@ -6,8 +6,16 @@ import EventNoteIcon from "@mui/icons-material/Event";
 import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
 import InputOption from "./InputOption";
 import "./Feed.css";
+import Post from "./Post";
 
 const Feed = () => {
+  const [posts, setPosts] = useState([])
+
+  const sendPost = e => {
+    e.preventDefault();
+    
+  };
+
   return (
     <div className="feed">
       <div className="feed__inputContainer">
@@ -15,7 +23,7 @@ const Feed = () => {
           <CreateIcon />
           <form>
             <input type="text" />
-            <button type="submit">Send</button>
+            <button onClick={sendPost} type="submit">Send</button>
           </form>
         </div>
         <div className="feed__inputOptions">
@@ -29,6 +37,11 @@ const Feed = () => {
           />
         </div>
       </div>
+      {/* Posts */}
+      {posts.map((post) => (
+        <Post />
+      ))}
+      < Post name="Gracious Nguwo" description="Learning from the best" message="Coding is fun!!!!" />
     </div>
   );
 };
